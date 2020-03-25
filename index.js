3cbbccb981d4c280c6417ae35ea6349c2ec72b4f
Jonathan Hsu <jonhsu.design@gmail.com>
2020-03-25 13:55:05 -0700

    finished mvp and stretch goals

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name,
+  this.age = age,
+  this.stomach = []
+}
+Person.prototype.eat = function(food) {
+  if(this.stomach.length <= 9){
+    this.stomach.push(food);
+  }
+}
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,8 +70,24 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model,
+  this.milesPerGallon = milesPerGallon,
+  this.tank = 0,
+  this.odometer = 0
+}
+Car.prototype.fill = function(gallons) {
+  return this.tank += gallons;
+}
+Car.prototype.drive = function(distance) {
+  if(this.tank >= (distance / this.milesPerGallon)){
+    this.odometer += distance;
+    this.tank -= (distance / this.milesPerGallon);
+  } else {
+    this.tank = 0;
+    this.odometer += distance - 1;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
 }
 
 /*
@@ -68,18 +97,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+}
 /* 
   TASK 4
-
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+
+  1. In the global scope, `this` will reference the window/console object
+
+  2. For functions called by objects using the `dot`, `this` will refer to the object on the left of the dot (implicit binding)
+
+  3. In constructor functions, `this` will reference the instance of the object created using the constructor (New binding)
+
+  4. For `.call` or `.apply` methods, `this` will be explicitly determined (Explicity binding)
 */
 
 
